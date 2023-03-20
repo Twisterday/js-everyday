d3e94e02091286e6f381dee799e406985040c9d0
Twisterday <garayda.rostik@gmail.com>
2023-03-20 00:34:56 -0600

    add solotions to p3 & p5 to task-4

--- js-everyday/task-4/js/main4.js
+++ js-everyday/task-4/js/main4.js
@@ -11,32 +11,61 @@ function sumSliceArray(arr, first, second) {
 
 // p2
 function checkAge() {
-	let name;
-	let age;
-	let status;
+	let name, age, status;
 
-	name = prompt("Enter your name:");
-	name = prompt("Enter your age:");
-	name = prompt("Enter your status (admin, moderator, user):");
+	name = prompt('Enter your name:');
+	age = prompt('Enter your age:');
+	status = prompt('Enter your status (admin, moderator, user):');
 
-	if (!name, !age, !status) {
-		throw new Error('The field is empty! Please enter your age');
-	} else {
-		alert(`Welcome, ${name}! You can now watch the movie.`);
+	if (!name || !age || !status) {
+		throw new Error('The field is empty! Please enter your information.');
 	}
+
 	if (age < 18 || age > 70) {
 		throw new RangeError('Age is out of range!');
-	} else {
-		alert(`Welcome, ${name}! You can now watch the movie.`);
 	}
 
 	const validSts = ['admin', 'moderator', 'user'];
 
 	if (!validSts.includes(status)) {
 		throw new EvalError('Invalid status value!');
-	} else {
-		alert(`Welcome, ${name}! You can now watch the movie.`);
 	}
 
-
+	alert(`Welcome, ${name}! You can now watch the movie.`);
 }
+
+// p3
+function calcRectangleArea(width, height) {
+	if (typeof width !== 'number' || typeof height !== 'number') {
+		throw new TypeError('Please enter numbers only');
+	}
+	const area = width * height;
+
+	return area;
+}
+
+// p4
+
+// p5
+function showUser(id) {
+	if (id < 0) {
+		throw new Error('Id must be a positive number');
+	}
+	return { id };
+}
+
+function showUsers(ids) {
+	const result = [];
+
+	for (const id of ids) {
+		try {
+			const user = showUser(id);
+			result.push(user);
+		} catch (error) {
+			console.log(error.message);
+		}
+	}
+
+	return result;
+}
+
